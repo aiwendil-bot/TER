a5fc6d942400b4b6fca25c818e59b82644778960
adrien <adriencallico@hotmail.fr>
2022-04-01 14:46:44 +0200

    graphiques methode review

--- methode 2/methode_2_review.jl
+++ methode 2/methode_2_review.jl
@@ -31,8 +31,8 @@ function methode_2_review(x::Float64,m::Int64,bornes::Vector{Float64})
     model::Model = Model(GLPK.Optimizer)
     #model::Model = Model(SCIP.Optimizer)              M = m + 1
     @variable(model, d[1:(m-1)] >= 0)
-    @variable(model, z[1:m])
-    @variable(model, u[1:m], Bin)
+    @variable(model, z[1:m-1])
+    @variable(model, u[1:m-1], Bin)
 
     #contraintes partie convexe
     @constraint(model, x  + sum(d[l] for l in 1:(m -2)) >= breakpoints[m-1])
@@ -59,8 +59,16 @@ function methode_2_review(x::Float64,m::Int64,bornes::Vector{Float64})
         L_f_x += sum(((s[k] - s[k-1])*(x - breakpoints[k] + sum(value(d[l]) for l in 1:(k-1)))) for k in convexe )
     end
     if !isempty(concave)    
-        L_f_x += 0.5* sum(((s[k] - s[k-1])*(x -2*value(z[k]) +2*breakpoints[k]*value(u[k]) - breakpoints[k] )) for k in concave)
+        L_f_x += 0.5*sum(((s[k] - s[k-1])*(x -2*value(z[k]) +2*breakpoints[k]*value(u[k]) - breakpoints[k] )) for k in concave)
     end    
     return L_f_x    
 end
 
+points_approches = Vector{Float64}(undef,0)
+
+for x in 1:0.01:2
+    push!(points_approches,methode_2_review(x,5,[1.0,2.0]))
+end
+f(x) = x->x^3-2*x^2-x+1
+
+plot([k for k in 1:0.01:2],[points_approches,f])
